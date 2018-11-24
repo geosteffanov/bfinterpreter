739bf0ee9531f5700c940a47068b2cff4473e894
Georgi Stefanov <georgi.stefanov@uber.com>
2018-11-24 16:00:37 +0200

    Add makefile and clean comments

--- main.go
+++ main.go
@@ -3,23 +3,6 @@ package main
 import "github.com/geosteffanov/bfinterpreter/bf"
 
 func main() {
-	//reader  := bufio.NewReader(os.Stdin)
-	//
-	//input, err := reader.ReadString('\n')
-	//
-	//fmt.Println(input)
-	//fmt.Println(err)
-	//interpreter := bf.NewInterpreter(",.")
-	//bf.Run(interpreter)
-
 	repl := bf.NewRepl()
 	repl.Start()
-	//writer := bufio.NewWriter(os.Stdout)
-	//
-	//for {
-	//	writer.Write([]byte("wow"))
-	//	writer.Flush()
-	//	time.Sleep(4 * time.Second)
-	//}
-
 }
